5f8674cf3ab5f9a74235aba396d5a9175110561b
Eleni Richey <eleni.richey@gmailcom>
2022-11-08 20:09:27 -0800

    Add sum function

--- arithmetic.py
+++ arithmetic.py
@@ -3,13 +3,13 @@
 
 def add(num1, num2):
     """Return the sum of num1 and num2."""
-
-    return 10
+    addition = num1 + num2
+    return addition
 
 
 def subtract(num1, num2):
     """Return the value of num1 minus num2."""
-
+   
 
 def multiply(num1, num2):
     """Multiply the num1 by num2 and return the result."""
